eb67e78b0ae1d8de3ce4c45e09135698ee8134d0
Muath Hassoun <moath.hassoun@gmail.com>
2025-07-21 23:29:02 +0300

    1.5.8... The STN program for display notifications is ready to use.

--- src/main/java/com/notifications/system_tray_notifications/system_tray/SystemTrayNotification.java
+++ src/main/java/com/notifications/system_tray_notifications/system_tray/SystemTrayNotification.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2025 Muath O. Y. Hassoun
+ *
+ * This file is part of the System Tray Notifications project.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.notifications.system_tray_notifications.system_tray;
 import static com.notifications.system_tray_notifications.influence.DisplayMessages.printErrorMessage;
 import com.notifications.system_tray_notifications.basics.AlarmSounds;
